ee74b2b44471f2d9c81b9f8ebc71876122a3b246
EllenPinheiro <ellen.pinheiro2@estudante.ifms.edu.br>
2023-11-21 09:00:03 -0400

    arrumando o código

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, {Request} from 'express';
 import BancoMongoDB from './infra/banco/banco-mongodb';
 import ListarFilme from './aplicacao/listar-filme.use-case';
-import SalvarFilme from './aplicacao/salva-filme.use-case'
+import SalvarFilme from './aplicacao/salva-filme.use-case';
+import cors from 'cors'
 const bancoMongoDB = new BancoMongoDB()
 const app = express();
 app.use(express.json())
-
+app.use(cors())
 
 app.get('/filmes', async (req, res) => {
     const listarFilme = new ListarFilme(bancoMongoDB)
@@ -13,26 +14,29 @@ app.get('/filmes', async (req, res) => {
     res.status(200).send(filmes)        
 });
 
-
-
-
-app.post('/filmes', (req:Request, res) => {
-    try{const {id, titulo, descricao, foto} = req.body
-    const filme:Filme = {
-        id,
-        titulo,
-        descricao,
-        foto,
+app.post('/filmes', async (req: Request, res) => {
+    try {
+        const { id, titulo, descricao, foto } = req.body;
+        const filme: Filme = {
+            id,
+            titulo,
+            descricao,
+            foto,
+        };
+        
+        const salvarFilme = new SalvarFilme(bancoMongoDB);
+        const filmes = await salvarFilme.execute(filme);
+        
+        const repeticaodecadastro = filmes_repositorio.find(filme => filme.id === id);
+        if (repeticaodecadastro) {
+            return res.status(400).send({ error: "Filme já cadastrado" });
+        }
+        
+        filmes_repositorio.push(filme);
+        res.status(201).send(filmes);
+    } catch (error) {
+        res.status(500).send({ error: "Ocorreu um erro ao processar a requisição" });
     }
-    const salvarFilme = new SalvarFilme(bancoMongoDB)
-    const filmes = await salvarFilme.execute(filme)
-    const repeticaodecadastro = filmes_repositorio.find(filme => filme.id === id)
-    if(repeticaodecadastro){
-        return res.status(400).send({error: "Filme já cadastrado"});
-    }
-    filmes_repositorio.push(filme)
-    res.status(201).send(filmes)
-}
 });
 
 app.delete('/filmes/:id', (req, res) => {
